45566e8175e6d5c40972005736085fe94f39713b
Jibin Thomas <jibin.thomas2706@gmail.com>
2023-07-24 17:28:56 +0530

    feat: convert `Operation` to typescript

--- src/lib/Operation.js
+++
@@ -1,9 +0,0 @@
-﻿export default class Operation {
-	constructor(action, startInOld, endInOld, startInNew, endInNew) {
-		this.action = action
-		this.startInOld = startInOld
-		this.endInOld = endInOld
-		this.startInNew = startInNew
-		this.endInNew = endInNew
-	}
-}

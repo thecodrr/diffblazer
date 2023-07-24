3c1b0bdf42c25cafd3602cec1173ba2acbafd539
Jibin Thomas <jibin.thomas2706@gmail.com>
2023-07-24 11:48:00 +0530

    chore: add build & publish commands

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import { defineConfig } from 'vite'
 export default defineConfig({
 	build: {
 		lib: {
-			entry: resolve(__dirname, 'src/lib/diff.js'),
+			entry: resolve(__dirname, 'src/lib/Diff.js'),
 			name: 'HtmlDiff',
+			fileName: 'htmldiff',
 			formats: ['es', 'umd'],
 		},
 		minify: false,
